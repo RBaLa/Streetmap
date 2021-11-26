99b6b62035c02c8c51a523a91702119fe6d0e92f
Rahul Balamurugan <r2balamurugan@uwaterloo.ca>
2021-11-26 19:00:26 +0000

    Update ece650-a3.cpp

--- a3/ece650-a3.cpp
+++ a3/ece650-a3.cpp
@@ -1,8 +1,70 @@
 #include <iostream>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
+using namespace std;
 
-int main (int argc, char **argv) {
-    std::cout << "Replace with your solution for the main "
-              << "driver program of Assignment 3\n";
+int main(int argc, char** argv){
+    
+    pid_t child_rgen,child_a1,child_a2,child_read;
+    int pipes[4];
+    pipe(pipes);
+    pipe(pipes+2);
+    int status;
+    
+    child_rgen = fork();
+    if (child_rgen==0){
+        dup2(pipes[1],STDOUT_FILENO);
+        close(pipes[0]);
+        close(pipes[1]);
+        close(pipes[2]);
+        close(pipes[3]);
+        execv("./rgen",argv);
+    }
+    else{
+        child_a1 = fork();
+        if (child_a1==0){
+            dup2(pipes[0],STDIN_FILENO);
+            close(pipes[1]);
+            close(pipes[0]);
+            dup2(pipes[3],STDOUT_FILENO);
+            close(pipes[2]);
+            close(pipes[3]);
+            exec("./ece650-a1");
+        }
+        else{
+            child_a2 = fork();
+            if (child_a2==0){
+                dup2(pipes[2],STDIN_FILENO);
+                close(pipes[3]);
+                close(pipe2[2]);
+                close(pipes[1]);
+                close(pipes[0]);
+                exec("./ece650-a2");
+            }
+            else{
+                child_read = fork();
+                if (child_read==0){
+                    dup2(pipes[3],STDOUT_FILENO);
+                    close(pipes[2]);
+                    close(pipes[1]);
+                    close(pipes[0]);
+                    close(pipes[3]);
+                    exec("./readinput");
+                }
+                else{
+                    close(pipes[0]);
+                    close(pipes[1]);
+                    close(pipes[2]);
+                    close(pipes[3]);
+                    
+                    for (unsigned i = 0; i < 3; i++)
+                        wait(&status);
+                }
+            }
+        }
+    }
     return 0;
 }
