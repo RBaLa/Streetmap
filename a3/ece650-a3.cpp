a79a685194c16f43db2beee34133ac54ea998733
Rahul Balamurugan <r2balamurugan@uwaterloo.ca>
2021-11-27 20:47:02 +0000

    Update ece650-a3.cpp

--- a3/ece650-a3.cpp
+++ a3/ece650-a3.cpp
@@ -15,21 +15,21 @@ int main(int argc, char** argv){
     
     child_rgen = fork();
     if (child_rgen==0){
-        dup2(pipe1[1],STDOUT_FILENO);
-        //close(pipe1[0]);
+        //dup2(pipe1[1],STDOUT_FILENO);
+        close(pipe1[0]);
         close(pipe1[1]);
-        //close(pipe2[0]);
-        //close(pipe2[1]);
+        close(pipe2[0]);
+        close(pipe2[1]);
         execv("./rgen",argv);
     }
     else{
         child_a1 = fork();
         if (child_a1==0){
             dup2(pipe1[0],STDIN_FILENO);
-            //close(pipe1[1]);
+            close(pipe1[1]);
             close(pipe1[0]);
             dup2(pipe2[1],STDOUT_FILENO);
-            //close(pipe2[0]);
+            close(pipe2[0]);
             close(pipe2[1]);
             execlp("python","python","ece650-a1.py",(char*)NULL);
         }
@@ -37,17 +37,17 @@ int main(int argc, char** argv){
             child_a2 = fork();
             if (child_a2==0){
                 dup2(pipe2[0],STDIN_FILENO);
-                //close(pipe2[1]);
+                close(pipe2[1]);
                 close(pipe2[0]);
-                //close(pipe1[1]);
-                //close(pipe1[0]);
+                close(pipe1[1]);
+                close(pipe1[0]);
                 execlp("./ece650-a2",(char*)NULL);
             }
             else{
                 dup2(pipe2[1],STDOUT_FILENO);
-                //close(pipe1[0]);
-                //close(pipe1[1]);
-                //close(pipe2[0]);
+                close(pipe1[0]);
+                close(pipe1[1]);
+                close(pipe2[0]);
                 close(pipe2[1]);
                 string line;
                 while (!cin.eof()){
