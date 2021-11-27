6477946b23380949e04b523c62890c834470c7f4
Rahul Balamurugan <r2balamurugan@uwaterloo.ca>
2021-11-27 23:13:27 +0000

    Update ece650-a3.cpp

--- a3/ece650-a3.cpp
+++ a3/ece650-a3.cpp
@@ -4,6 +4,9 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+#define READ_END 0
+#define WRITE_END 1
+
 using namespace std;
 
 int main(int argc, char** argv){
@@ -15,44 +18,46 @@ int main(int argc, char** argv){
     
     child_rgen = fork();
     if (child_rgen==0){
-        int dup2check1 = dup2(pipe1[1],STDOUT_FILENO);
+        cerr<<"pipe1 write Before dup2:"<<pipe1[WRITE_END]<<endl;
+        int dup2check1 = dup2(pipe1[WRITE_END],STDOUT_FILENO);
         cerr<<"dup2 check rgen:"<<dup2check1<<endl;
-        close(pipe1[0]);
-        close(pipe1[1]);
-        close(pipe2[0]);
-        close(pipe2[1]);
+        cerr<<"pipe1 write after dup2"<<pipe1[WRITE_END]<<endl;
+        close(pipe1[READ_END]);
+        close(pipe1[WRITE_END]);
+        close(pipe2[READ_END]);
+        close(pipe2[WRITE_END]);
         execv("./rgen",argv);
     }
     else{
         child_a1 = fork();
         if (child_a1==0){
-            int dup2check2 = dup2(pipe1[0],STDIN_FILENO);
-            cerr<<"dup2 check a1 read:"<<dup2check2<<endl;
-            close(pipe1[1]);
-            close(pipe1[0]);
-            int dup2check3 = dup2(pipe2[1],STDOUT_FILENO);
-            cerr<<"dup2 check a1 write:"<<dup2check3<<endl;
-            close(pipe2[0]);
-            close(pipe2[1]);
+            int dup2check2 = dup2(pipe1[READ_END],STDIN_FILENO);
+            //cerr<<"dup2 check a1 read:"<<dup2check2<<endl;
+            close(pipe1[WRITE_END]);
+            close(pipe1[READ_END]);
+            int dup2check3 = dup2(pipe2[WRITE_END],STDOUT_FILENO);
+            //cerr<<"dup2 check a1 write:"<<dup2check3<<endl;
+            close(pipe2[READ_END]);
+            close(pipe2[WRITE_END]);
             execlp("python","python","ece650-a1.py",(char*)NULL);
         }
         else{
             child_a2 = fork();
             if (child_a2==0){
-                int dup2check4 = dup2(pipe2[0],STDIN_FILENO);
-                cerr<<"dup2 check a2:"<<dup2check4<<endl;
-                close(pipe2[1]);
-                close(pipe2[0]);
-                close(pipe1[1]);
-                close(pipe1[0]);
+                int dup2check4 = dup2(pipe2[READ_END],STDIN_FILENO);
+                //cerr<<"dup2 check a2:"<<dup2check4<<endl;
+                close(pipe2[WRITE_END]);
+                close(pipe2[READ_END]);
+                close(pipe1[WRITE_END]);
+                close(pipe1[READ_END]);
                 execlp("./ece650-a2",(char*)NULL);
             }
             else{
-                dup2(pipe2[1],STDOUT_FILENO);
-                close(pipe1[0]);
-                close(pipe1[1]);
-                close(pipe2[0]);
-                close(pipe2[1]);
+                dup2(pipe2[WRITE_END],STDOUT_FILENO);
+                close(pipe1[READ_END]);
+                close(pipe1[WRITE_END]);
+                close(pipe2[READ_END]);
+                close(pipe2[WRITE_END]);
                 string line;
                 while (!cin.eof()){
                     getline(cin,line);
