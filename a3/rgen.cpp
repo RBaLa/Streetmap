d1061009a2d93cb52ffebe54ee188307bed8e2a3
Rahul Balamurugan <r2balamurugan@uwaterloo.ca>
2021-11-28 21:49:01 +0000

    Update rgen.cpp

--- a3/rgen.cpp
+++ a3/rgen.cpp
@@ -156,9 +156,10 @@ int main(int argc, char** argv){
     auto t2 = high_resolution_clock::now();
     while(!cin.eof()){
         if (database.size()!=0){
-            for (unsigned i=0;i<database.size();i++){
+            /*for (unsigned i=0;i<database.size();i++){
                 cout<<"rm "<<"\""<<database[i].name<<"\""<<endl;
-            }
+            }*/
+            cout<<"rm \"all\""<<endl
             database.clear();
         }
         n_streets = d_n(urandom);
@@ -206,7 +207,6 @@ int main(int argc, char** argv){
             cout<<endl;
         }
         cout<<"gg\n";
-        cerr<<"this is to keep rgen honest before sleep\n";
         /*t2 = high_resolution_clock::now();
         auto ms_int = duration_cast<milliseconds>(t2 - t1);
         cerr<<"---RGEN: sent commands to A1. Time taken: "<<ms_int.count()<<"ms---\n";
