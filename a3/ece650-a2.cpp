54da41640a0767db6e5379a5b40874c17d421687
Rahul Balamurugan <r2balamurugan@uwaterloo.ca>
2021-11-28 00:25:55 +0000

    Update ece650-a2.cpp

--- a3/ece650-a2.cpp
+++ a3/ece650-a2.cpp
@@ -60,13 +60,11 @@ int main() {
     unsigned destination = 0;
     vector<unsigned> edgeValues;
 
-    
+    string line;
 
-    while (!cin.eof()) {
+    while (getline(cin, line)) {
         cerr<<"A2: Receiving input from A1: ";
-        string line;
         char command;
-        getline(cin, line);
         cerr<<line<<endl;
         command = line[0];
         if (command=='V'){
