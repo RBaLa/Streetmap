65bc099df27a856157b74151378c6804157ad2e1
Rahul Balamurugan <r2balamurugan@uwaterloo.ca>
2021-11-28 00:12:43 +0000

    Update ece650-a3.cpp

--- a3/ece650-a3.cpp
+++ a3/ece650-a3.cpp
@@ -44,18 +44,19 @@ int main(int argc, char** argv){
                 close(pipe2[READ_END]);
                 close(pipe1[WRITE_END]);
                 close(pipe1[READ_END]);
-                execlp("./ece650-a2",(char*)NULL);
+                execlp("./ece650-a2",NULL);
             }
             else{
-                //dup2(pipe2[WRITE_END],STDOUT_FILENO);
+                dup2(pipe2[WRITE_END],STDOUT_FILENO);
                 close(pipe1[READ_END]);
                 close(pipe1[WRITE_END]);
                 close(pipe2[READ_END]);
                 close(pipe2[WRITE_END]);
                 string line;
                 while (!cin.eof()){
-                    //getline(cin,line);
-                    //cout<<line<<endl;
+                    getline(cin,line)
+                    if (line.size()>1)
+                        cout<<line<<endl;
                     if (cin.eof())
                         break;
                 }
